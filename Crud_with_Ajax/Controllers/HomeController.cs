b8d828db6164d38d59e13db94279e395c6164055
designer-haxxan <ahmadhassan4622@gmail.com>
2023-02-27 23:39:48 +0500

    retrieve dtaa using ajax complete

--- Crud_with_Ajax/Controllers/HomeController.cs
+++ Crud_with_Ajax/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Crud_with_Ajax.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,25 @@ namespace Crud_with_Ajax.Controllers
 {
     public class HomeController : Controller
     {
+
+        crud_ajaxEntities db = new crud_ajaxEntities();
         public ActionResult Index()
         {
+            List<tbl_depart> deplis = db.tbl_depart.ToList();
+            ViewBag.Departlist = new SelectList(deplis, "id", "name");
             return View();
         }
-
+        public JsonResult GetStudentList()
+        {
+            List<StudentViewModel> std = db.tbl_students.Where(x => x.isdeleted == false).Select(x => new StudentViewModel {
+                id = x.id,
+                name = x.name,
+                email = x.email,
+                dpname=x.tbl_depart.name
+            }
+            ).ToList();
+            return Json(std,JsonRequestBehavior.AllowGet);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ Crud_with_Ajax/Models/StudentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Crud_with_Ajax.Models
+{
+    public class StudentViewModel
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public Nullable<bool> isdeleted { get; set; }
+        public Nullable<long> dpt { get; set; }
+
+        public string dpname { get; set; }
+
+      
+    }
+}
